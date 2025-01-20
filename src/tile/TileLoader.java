e4a35f38a84c43b17009c0b3517ba07b8ad7d993
dgr75 <dgr75@cornell.edu>
2025-01-19 20:44:54 -0500

    Updated TileLoader for OS compatability

--- src/tile/TileLoader.java
+++ src/tile/TileLoader.java
@@ -5,6 +5,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 
 public class TileLoader {
@@ -38,6 +40,7 @@ public class TileLoader {
 
         File[] files = folder.listFiles();
         if (files != null) {
+            Arrays.sort(files, Comparator.comparing(File::getName)); // Sort files for OS consistency
             int i = 1;
             for (File file : files) {
                 try {
